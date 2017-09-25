945ea38b41797de24e651b4bb9b21029c8fbdc42
Peter Lai <milk4candy@gmail.com.com>
2017-09-25 16:38:51 +0800

    raise temperature limit to 92C

--- miner/EWBF/restart_ewbf_container.sh
+++ miner/EWBF/restart_ewbf_container.sh
@@ -4,5 +4,5 @@ docker stop ewbf
 docker container prune -f
 
 nvidia-docker run -td --name ewbf \
-milk4candy/ewbf0.3.4b:17.9.1 \
+milk4candy/ewbf0.3.4b:17.9.2 \
 t1LcrECyQ6BToCXxsp4EkTNz74h7DakjsuN.${HOSTNAME}/milk4candy@gmail.com
--- miner/EWBF/run_ewbf_container.sh
+++ miner/EWBF/run_ewbf_container.sh
@@ -3,5 +3,5 @@
 docker container prune -f
 
 nvidia-docker run -td --name ewbf \
-milk4candy/ewbf0.3.4b:17.9.1 \
+milk4candy/ewbf0.3.4b:17.9.2 \
 t1LcrECyQ6BToCXxsp4EkTNz74h7DakjsuN.${HOSTNAME}/milk4candy@gmail.com
